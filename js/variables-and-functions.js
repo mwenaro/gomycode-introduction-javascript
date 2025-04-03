dd2461c11e8f982d0294748bcb435b0626520843
Mwero Abdalla <mwerothewebmaker@gmail.com>
2025-04-03 21:28:33 +0300

    chore: add variables-and-functions.js. Add variable declaration with var, let , const

---
+++ js/variables-and-functions.js
@@ -0,0 +1,19 @@
+// variables
+let surname  //declaration
+const  DISCOUNT_RATE = 0.09
+
+const numbers = [12, 45]
+
+surname = 'Mwero' // assignmemt -> Initiolization 
+
+let isAdmin = false
+
+//key worsd used for declaration in js - 
+// var,
+//  let, const
+
+console.log(surname) 
+surname = 'Rashid'
+console.log(surname)
+
+
