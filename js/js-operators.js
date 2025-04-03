f7d5dfa5ac204eef5be737aac558817548166d1e
Mwero Abdalla <mwerothewebmaker@gmail.com>
2025-04-03 19:52:45 +0300

    chore: add js/js-operators.js. Add arithmetic ops

---
+++ js/js-operators.js
@@ -0,0 +1,20 @@
+// operators
+/*
+    1. Arithmetic  
+        - + addtion
+        - - substraction
+        - * - multiplication
+        - / - division
+        - % - modulo
+        - ** - exponetial/ powers
+
+
+*/
+// examples of arithmetics
+let x = 5, y = 2;
+console.log(x + " + "+ y + " = " + (x + y))
+console.log(x + " - "+ y + " = " + (x - y))
+console.log(x + " * "+ y + " = " + (x * y))
+console.log(x + " / "+ y + " = " + (x / y))
+console.log(x + " % "+ y + " = " + (x % y))
+console.log(x + " ** "+ y + " = " + (x ** y))
