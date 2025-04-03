d2350b730761de9eba2dd79fbe04a6119504c9dc
Mwero Abdalla <mwerothewebmaker@gmail.com>
2025-04-03 21:54:15 +0300

    chore: update variables*.js. Add functioin and default params examples

--- js/variables-and-functions.js
+++ js/variables-and-functions.js
@@ -1,19 +1,54 @@
 // variables
-let surname  //declaration
-const  DISCOUNT_RATE = 0.09
+let surname; //declaration
+const DISCOUNT_RATE = 0.09;
 
-const numbers = [12, 45]
+const numbers = [12, 45];
 
-surname = 'Mwero' // assignmemt -> Initiolization 
+surname = "Mwero"; // assignmemt -> Initiolization
 
-let isAdmin = false
+let isAdmin = false;
 
-//key worsd used for declaration in js - 
+//key worsd used for declaration in js -
 // var,
 //  let, const
 
-console.log(surname) 
-surname = 'Rashid'
-console.log(surname)
+console.log(surname);
+surname = "Rashid";
+console.log(surname);
 
+// functions
+function greet() {
+  console.log("Hello ");
+  console.log("How old are you?");
+  console.log("------------------------");
+}
 
+function greetPeople(name) {
+  console.log("Hello " + name);
+  console.log("How old are you?");
+  console.log("------------------------");
+}
+
+// greetPeople('Mwero')
+
+// greetPeople('John')
+
+// greetPeople('Jane')
+
+function add(num1, num2) {
+  // let result = num1 + num2
+  // // console.log(result)
+  return num1 + num2;
+}
+
+const sum = add(5, 2);
+console.log(sum);
+
+// default params
+function area(len, wid = 0) {
+  if (wid === 0) return len ** 2;
+
+  return len * wid;
+}
+
+console.log(area(10, 3));
