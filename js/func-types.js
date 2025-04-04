f0d9dc6eeb51b4fc55f63fa5328b5d1229a84faf
Mwero Abdalla <mwerothewebmaker@gmail.com>
2025-04-04 20:56:42 +0300

    chore: add func-types.js. Explain function types

---
+++ js/func-types.js
@@ -0,0 +1,54 @@
+// functions
+// 1. function declaration
+/*
+    function add(n, m){
+    return n +m
+    }
+
+2. function expression
+const cube = function (n){
+return n * n * n
+}
+
+3. callback function
+    - a func that is received by another function as an argument
+
+4. H.O.F 
+  - A function that accepts another function as an argument inorder to either call or return it.
+
+5. IIFE - Imediately Invoked Function Exprress
+  - Is an automatically invoked function
+*/
+
+// funcion express
+const cube = function (n) {
+  return n * n * n;
+};
+
+console.log(cube(1));
+
+//callback
+function gradingSystem(mark, cb) {
+  const grade = cb(mark);
+
+  console.log(`You scored ${mark}% which is grade ${grade}`);
+}
+
+function getGrade(score) {
+  if (score >= 70) return "A";
+  if (score >= 60) return "B";
+  if (score >= 50) return "C";
+  if (score >= 40) return "D";
+  return "F";
+}
+
+// gradingSystem(30, getGrade);
+
+// IIFE
+(
+    (val) => {
+
+  console.log("Hello from IIFE");
+  console.log(val);
+}
+)(45);
